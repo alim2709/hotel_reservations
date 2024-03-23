aaa13cd8a698a19946cc83d60473e53855d75d4f
alim2709 <alimselemetow@gmail.com>
2024-03-23 23:19:45 +0200

    Change queue imports in app/migrations/env.py file

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -9,12 +9,12 @@ from alembic import context
 
 sys.path.insert(0, dirname(dirname(dirname(abspath(__file__)))))
 
-from app.database import Base
+from app.bookings.models import Bookings  # noqa
 from app.config import settings
-from app.hotels.models import Hotels
-from app.hotels.rooms.models import Rooms
-from app.bookings.models import Bookings
-from app.users.models import Users
+from app.database import Base
+from app.hotels.models import Hotels  # noqa
+from app.hotels.rooms.models import Rooms  # noqa
+from app.users.models import Users  # noqa
 
 
 # this is the Alembic Config object, which provides
